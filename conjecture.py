3e8d9d10405c91a55e6987e3fdeebb098f1eb39f
Yoram van de Velde <yoram@rootnet.nl>
2021-12-10 18:45:53 +0100

    Initial commit

---
+++ conjecture.py
@@ -0,0 +1,24 @@
+# Collatz conjecture
+# Seeming randomness in mathematics
+# aka  : 3x+1
+# video: https://www.youtube.com/watch?v=094y1Z2wpJg
+
+from random import randrange
+num = randrange(10000)
+startnum = num
+count = 0
+
+print("\n=== Collatz conjecture. Using {0} ===".format(num))
+
+while True:
+  if (num % 2) == 0:
+    num = num / 2
+    print("\033[91m{0}\033[0m".format(num))
+  else:
+    num = (num * 3) + 1
+    print("\033[1m{0}\033[0m".format(num))
+  
+  if num == 1:
+    print("\nThe number {0} reached 1 in {1} steps, stopping to not loop forever.\n".format(startnum,count))
+    break
+  count += 1
