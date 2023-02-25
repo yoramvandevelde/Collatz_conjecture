dd78c32f9facca6a4f3c8535ffa1590ff7520d3e
sp <_@sp2.io>
2023-02-25 22:43:48 +0100

    adds an initial Rust implementation

---
+++ rust/src/main.rs
@@ -0,0 +1,36 @@
+
+// Collatz conjecture
+// Seeming randomness in mathematics
+// aka  : 3x+1
+// video: https://www.youtube.com/watch?v=094y1Z2wpJg
+
+use rand::Rng;
+
+fn main() {
+  let mut rng = rand::thread_rng();
+  let mut num = rng.gen_range(0..10000);
+  let startnum = num;
+  let mut count = 0; 
+
+  println!("\n=== Collatz conjecture. Using {} ===", startnum);
+  
+  loop {
+    if num % 2 == 0 {
+      num /= 2;
+      
+      // Using hexadecimal escape characters because Rust doesn't 
+      // support octal escape chars
+      println!("\x1b[91m{}\x1b[0m", num)
+    } else {
+      num = (num * 3) + 1;
+      println!("\x1b[1m{}\x1b[0m", num)
+    }
+
+    count += 1;
+  
+    if num == 1 {
+      println!("\nThe number {} reached 1 in {} steps, stopping to not loop forever.\n", startnum, count);
+      break;
+    }
+  }
+}
